558e852f4c4576252f62c92d3ca820cd5ba535cf
InnetaSh <inneta3@gmail.com>
2025-09-09 18:57:51 +0300

    add html page

---
+++ web/wwwroot/appDesign.js
@@ -0,0 +1,28 @@
+// JavaScript source code
+(async function () {
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const defaultImage = "https://gateway.pinata.cloud/ipfs/bafkreiesrks5z3a4rkskyr7hmqmay7woqxnu76e57sc5sdat2kuq2h57zm";
+
+        const avatarImg = document.getElementById('petAvatarSelect');
+        const loader = document.getElementById('avatarLoader');
+        
+        loader.style.display = 'block';
+        avatarImg.style.display = 'none';
+
+        avatarImg.src = defaultImage;
+        
+        avatarImg.addEventListener('load', () => {
+            loader.style.display = 'none';
+            avatarImg.style.display = 'block';
+        });
+        
+        avatarImg.addEventListener('error', () => {
+            loader.style.display = 'none';
+            avatarImg.alt = 'Ошибка загрузки аватара';
+            avatarImg.style.display = 'block';
+        });
+    });
+
+
+})();
